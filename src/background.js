79835d409a23e86c735158b468dc717ee5a9ba98
koltonostler <kolton.ostler@gmail.com>
2022-11-26 19:57:17 -0700

    rebuilt time-manager with chrome-extension cli
    because of limits of importing npm packages, I decided to rebuild my time-manager chrome extension so that I could use charts.js library for my charts

--- src/background.js
+++ src/background.js
@@ -1,21 +1,296 @@
-'use strict';
-
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
-
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
+const onUpdate = (tabId, info, tab) =>
+  /^https?:/.test(info.url) && findTab([tab]);
+findTab();
+chrome.runtime.onConnect.addListener((port) => {
+  if (port.name === 'keepAlive') {
+    setTimeout(() => port.disconnect(), 250e3);
+    port.onDisconnect.addListener(() => findTab());
   }
+});
+async function findTab(tabs) {
+  if (chrome.runtime.lastError) {
+    /* tab was closed before setTimeout ran */
+  }
+  for (const { id: tabId } of tabs ||
+    (await chrome.tabs.query({ url: '*://*/*' }))) {
+    try {
+      await chrome.scripting.executeScript({
+        target: { tabId },
+        func: connect,
+      });
+      chrome.tabs.onUpdated.removeListener(onUpdate);
+      return;
+    } catch (activeInfo) {}
+  }
+  chrome.tabs.onUpdated.addListener(onUpdate);
+}
+function connect() {
+  chrome.runtime
+    .connect({ name: 'keepAlive' })
+    .onDisconnect.addListener(connect);
+}
+
+const urlIgnoreList = ['chrome://newtab/'];
+
+const openTabs = {};
+
+let trackActiveOnly = true;
+let lastTab;
+
+// setup Tab class
+class Tab {
+  // constructor for class
+  constructor(tabId, windowId, url, icon, audible, budget) {
+    this.tabId = tabId;
+    this.windowId = windowId;
+    this.url = url;
+    this.icon = icon;
+    this.isAudible = audible;
+    this.budget = budget;
+    this.timeStamp = new Date().toDateString();
+    this.start = Date.now();
+    this.end = null;
+  }
+
+  // function that will return true/false on if the site is blocked.
+  async isBlocked() {
+    let domain = getDomain(this.url);
+    let date = this.timeStamp;
+    let budgetedTime = this.budget;
+    let promise = new Promise((resolve) => {
+      if (domain) {
+        // get stored data for domain
+        chrome.storage.sync.get(domain, (res) => {
+          // if the domain has no data or there is no budget for the domain, return false
+          if (
+            Object.keys(res).length === 0 ||
+            Object.keys(res[domain]).length === 0 ||
+            budgetedTime === null
+          ) {
+            resolve(false);
+            return;
+          }
+          const domainTime = res[domain][date];
+          // check to see if current day time spent at domain is greater than budgeted time.  if so return true
+          if (domainTime > budgetedTime) {
+            resolve(true);
+          } else {
+            resolve(false);
+          }
+        });
+      }
+    });
+    let result = await promise;
+    return result;
+  }
+
+  setStartTime() {
+    this.start = Date.now();
+  }
+
+  setCloseTime() {
+    this.end = Date.now();
+    this.calcTimeOpen();
+  }
+
+  // function that calculates the time the current tab was open for using start and end times
+  calcTimeOpen() {
+    const timeOpenMilli = this.end - this.start;
+    const timeOpenSec = timeOpenMilli / 1000;
+    console.log(`${this.url} was open for ${timeOpenSec} seconds`);
+    this.updateStorage(timeOpenSec);
+  }
+
+  //  function that will update storage with the data from calcTimeOpen;
+  async updateStorage(timeOpen) {
+    if (
+      !this.url ||
+      urlIgnoreList.includes(this.url) ||
+      (await this.isBlocked())
+    ) {
+      // if url does not exist or is in urlIgnoreList or isBlocked, then do nothing
+    } else {
+      const domain = getDomain(this.url);
+      // get data from storage for current domain
+      await chrome.storage.sync.get(domain, (res) => {
+        const fullDate = new Date().toDateString();
+        // if domain has no data stored, just store the current data
+        if (Object.keys(res).length === 0) {
+          chrome.storage.sync.set({ [domain]: { [fullDate]: timeOpen } });
+        } else {
+          let newData = res[domain];
+          // if date already exists, add the timeOpen to the existing time.
+          if (fullDate in newData) {
+            const oldTime = parseFloat(res[domain][fullDate]);
+            const newTime = oldTime + parseFloat(timeOpen);
+            newData[fullDate] = newTime;
+          } else {
+            newData[fullDate] = timeOpen;
+          }
+          // set the new data to storage
+          chrome.storage.sync.set({ [domain]: newData });
+        }
+      });
+    }
+  }
+}
+
+async function getBudget(url) {
+  const domain = getDomain(url);
+  if (domain) {
+    let budget = null;
+    // get domain data from storage
+    const data = await chrome.storage.sync.get(domain);
+    // if domain has data, get the budget
+    if (data[domain]) {
+      if ('budget' in data[domain]) {
+        budget = data[domain]['budget'];
+      }
+    }
+    return budget;
+  }
+}
+
+// gets the active tab.
+async function getActiveTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+// get all active tabs
+async function getActiveTabs() {
+  let queryOptions = { active: true };
+  let tabs = await chrome.tabs.query(queryOptions);
+  return tabs;
+}
+
+async function createNewTab(tab) {
+  let budget = await getBudget(tab.url);
+  let currentTab = new Tab(
+    tab.id,
+    tab.windowId,
+    tab.url,
+    tab.favIconUrl,
+    tab.audible,
+    budget
+  );
+  openTabs[tab.id] = currentTab;
+  return currentTab;
+}
+// function that will record the tab and delete it from the openTabs object
+function recordAndCloseTab(tabId, windowId) {
+  if (tabId in openTabs && openTabs[tabId].windowId === windowId) {
+    openTabs[tabId].setCloseTime();
+    delete openTabs[tabId];
+  }
+}
+// function that verifies the url is valid and returns the domain.
+function getDomain(url) {
+  try {
+    const domain = new URL(url).hostname;
+    return domain;
+  } catch (err) {
+    return 'hello';
+  }
+}
+
+async function getActiveState() {
+  let activeState = await chrome.storage.sync.get('activeState');
+
+  return activeState.activeState;
+}
+
+function blockSite(budget, url) {
+  document.body.innerHTML = `<h1 class='block'> ${url} is blocked!!! </h1>
+                                <p>You have exceeded your budget of ${
+                                  budget / 60
+                                } min.</p>  
+    
+    `;
+}
+
+const blockPageCss =
+  'body {background-color: black !important; color: white !important}';
+
+async function blockedSiteCheck(tab) {
+  if (await tab.isBlocked()) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.tabId },
+      func: blockSite,
+      args: [tab.budget, tab.url],
+    });
+    chrome.scripting.insertCSS({
+      target: { tabId: tab.tabId },
+      css: blockPageCss,
+    });
+    return true;
+  }
+  return false;
+}
+
+/*   TAB LISTENERS   */
+
+// add Listener for page update
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete') {
+    recordAndCloseTab(tabId, tab.windowId);
+    lastTab = await createNewTab(tab);
+    blockedSiteCheck(lastTab);
+  }
+});
+
+// listener for closing a tab to setCloseTime only if the closed tab is the active tab
+chrome.tabs.onRemoved.addListener((tabId, info) => {
+  recordAndCloseTab(tabId, info.windowId);
+});
+// listener for changing tabs and then will close previous tab and set new current tab to active tab.
+chrome.tabs.onActivated.addListener(async (tabInfo) => {
+  // check to see if trackActiveOnly is toggled on
+  if (trackActiveOnly) {
+    // check to make sure there is a tab to close
+    if (lastTab !== undefined) {
+      // record and close tab
+      recordAndCloseTab(lastTab.tabId, tabInfo.windowId);
+    }
+  }
+  // check to see if tab was already open
+  if (tabInfo.tabId in openTabs) {
+    recordAndCloseTab(tabInfo.tabId, tabInfo.windowId);
+  }
+  let activeTab = await getActiveTab();
+
+  lastTab = await createNewTab(activeTab);
+
+  blockedSiteCheck(lastTab);
+});
+
+// chrome.tabs.onDetached.addListener((tabId, detachInfo) => {
+// 	console.log(detachInfo);
+// 	chrome.windows.get(detachInfo.oldWindowId, { populate: true }, async () => {
+// 		let activeTabs = await chrome.tabs.query({ active: true });
+
+// 		console.log(activeTabs);
+// 	});
+// });
+
+chrome.windows.onCreated.addListener(async () => {
+  let activeTabs = await getActiveTabs();
+  activeTabs.forEach(async (tab) => {
+    await createNewTab(tab);
+  });
+  trackActiveOnly = await getActiveState();
+});
+
+// function that validates if url is valid and returns the full domain from url
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if ('trackActive' in request) {
+    trackActiveOnly = request.trackActive;
+  }
+  sendResponse({ activeState: trackActiveOnly });
+
+  // Note: Returning true is required here!
+  //  ref: http://stackoverflow.com/questions/20077487/chrome-extension-message-passing-response-not-sent
+  return true;
 });
--- src/popup.js
+++ src/popup.js
@@ -1,112 +1,173 @@
 'use strict';
 
+import Chart from 'chart.js/auto';
 import './popup.css';
 
-(function () {
-  // We will make use of Storage API to get and store `count` value
-  // More information on Storage API can we found at
-  // https://developer.chrome.com/extensions/storage
+const selection = document.querySelector('#chart-selection');
+let chartSelection = 'top5';
 
-  // To get storage access, we have to mention it in `permissions` property of manifest.json file
-  // More information on Permissions can we found at
-  // https://developer.chrome.com/extensions/declare_permissions
-  const counterStorage = {
-    get: (cb) => {
-      chrome.storage.sync.get(['count'], (result) => {
-        cb(result.count);
-      });
-    },
-    set: (value, cb) => {
-      chrome.storage.sync.set(
-        {
-          count: value,
+async function getTodaysData() {
+  const response = await chrome.storage.sync.get(null);
+  return response;
+}
+
+getTodaysData().then((data) => {
+  let today = new Date().toDateString();
+  console.log(data);
+  const sortedData = Object.keys(data)
+    .filter((key) => {
+      if (key === 'activeState') {
+        return false;
+      }
+      return true;
+    })
+    .map((key) => {
+      return [key, data[key][today]];
+    })
+    .sort((a, b) => a[1] - b[1]);
+
+  let todaysData = sortedData.map((datapoint) => {
+    return datapoint[1];
+  });
+  let todaysLabels = sortedData.map((datapoint) => {
+    return datapoint[0];
+  });
+
+  const total = todaysData.reduce((a, b) => a + b, 0);
+
+  const chartData = {
+    labels: todaysLabels,
+    datasets: [
+      {
+        data: todaysData,
+        hoverOffset: 40,
+      },
+    ],
+  };
+
+  let options = {
+    plugins: {
+      legend: {
+        display: true,
+        reverse: true,
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            const totalSeconds = context.parsed;
+            const hours = Math.floor(totalSeconds / 3600);
+            const min = Math.floor((totalSeconds - hours * 3600) / 60);
+            let sec = totalSeconds - hours * 3600 - min * 60;
+            sec = Math.round(sec * 100) / 100;
+            sec = sec.toFixed(0);
+            let timeFormat = ` ${hours} hr ${min} min`;
+            if (hours < 1) {
+              if (min < 1) {
+                timeFormat = ` ${sec} sec`;
+              } else {
+                timeFormat = ` ${min} min ${sec} sec`;
+              }
+            }
+            return timeFormat;
+          },
+          afterLabel: function (context) {
+            const percentage = context.parsed / total;
+            return '(' + (percentage * 100).toFixed(2) + '%)';
+          },
         },
-        () => {
-          cb();
-        }
-      );
+      },
     },
   };
 
-  function setupCounter(initialValue = 0) {
-    document.getElementById('counter').innerHTML = initialValue;
+  let chart = new Chart(document.getElementById('acquisitions'), {
+    type: 'doughnut',
+    data: chartData,
+    options: options,
+  });
 
-    document.getElementById('incrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'INCREMENT',
+  selection.addEventListener('change', (e) => {
+    console.log('changed');
+    chartSelection = e.target.value;
+    if (chartSelection === 'top5') {
+      const top5 = sortedData.slice(sortedData.length - 5, sortedData.length);
+      todaysData = top5.map((datapoint) => {
+        return datapoint[1];
       });
-    });
-
-    document.getElementById('decrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'DECREMENT',
+      todaysLabels = top5.map((datapoint) => {
+        return datapoint[0];
       });
-    });
-  }
-
-  function updateCounter({ type }) {
-    counterStorage.get((count) => {
-      let newCount;
-
-      if (type === 'INCREMENT') {
-        newCount = count + 1;
-      } else if (type === 'DECREMENT') {
-        newCount = count - 1;
-      } else {
-        newCount = count;
-      }
-
-      counterStorage.set(newCount, () => {
-        document.getElementById('counter').innerHTML = newCount;
-
-        // Communicate with content script of
-        // active tab by sending a message
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          const tab = tabs[0];
-
-          chrome.tabs.sendMessage(
-            tab.id,
-            {
-              type: 'COUNT',
-              payload: {
-                count: newCount,
-              },
-            },
-            (response) => {
-              console.log('Current count value passed to contentScript file');
-            }
-          );
-        });
+    } else {
+      todaysData = sortedData.map((datapoint) => {
+        return datapoint[1];
       });
-    });
-  }
-
-  function restoreCounter() {
-    // Restore count value
-    counterStorage.get((count) => {
-      if (typeof count === 'undefined') {
-        // Set counter value as 0
-        counterStorage.set(0, () => {
-          setupCounter(0);
-        });
-      } else {
-        setupCounter(count);
-      }
-    });
-  }
-
-  document.addEventListener('DOMContentLoaded', restoreCounter);
-
-  // Communicate with background file by sending a message
-  chrome.runtime.sendMessage(
-    {
-      type: 'GREETINGS',
-      payload: {
-        message: 'Hello, my name is Pop. I am from Popup.',
-      },
-    },
-    (response) => {
-      console.log(response.message);
+      todaysLabels = sortedData.map((datapoint) => {
+        return datapoint[0];
+      });
     }
-  );
-})();
+    (chartData.datasets = [
+      {
+        data: todaysData,
+        hoverOffset: 40,
+      },
+    ]),
+      (chartData.labels = todaysLabels);
+    chart.update();
+  });
+});
+
+const clearButton = document.getElementById('clearBtn');
+
+clearButton.addEventListener('click', () => {
+  chrome.storage.sync.clear();
+});
+
+let toggleActive = { trackActive: true };
+
+const form = document.getElementById('timer-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let url = form.elements['url'].value;
+  let timer = form.elements['timer'].value;
+
+  setBudget(url, timer);
+  form.reset();
+});
+
+// function that will set the budget with inputs from set-timer form
+function setBudget(url, timer) {
+  // convert timer from minutes to seconds
+  timer = timer * 60;
+  // convert input to valid url
+  url = `https://${url}`;
+  let domain = new URL(url).hostname;
+  chrome.storage.sync.get(domain, (res) => {
+    // if domain does not already have data, just set the budget
+    if (Object.keys(res).length === 0) {
+      chrome.storage.sync.set({ [domain]: { budget: timer } });
+    } else {
+      // if the domain already has data, we need to add the 'budget' key with the new budget as the value
+      let newData = res[domain];
+      newData['budget'] = timer;
+      chrome.storage.sync.set({ [domain]: newData });
+    }
+  });
+}
+
+function saveActiveState() {
+  chrome.storage.sync.set({ ['activeState']: toggleActive.trackActive });
+}
+
+const toggle = document.getElementById('checkbox');
+
+toggle.addEventListener('change', () => {
+  toggleActive.trackActive = !toggleActive.trackActive;
+  saveActiveState();
+  chrome.runtime.sendMessage(toggleActive);
+});
+
+chrome.runtime.sendMessage({ msg: 'getActiveState' }, function (response) {
+  console.log(`message from background: ${JSON.stringify(response)}`);
+  toggleActive.trackActive = response.activeState;
+  toggle.checked = toggleActive.trackActive;
+});
